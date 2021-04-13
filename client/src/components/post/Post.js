c018e094643ee2fc9deecff99f930c0baebeef9f
Sandip Rijal <sandurijal03@hotmail.com>
2021-04-14 04:23:25 +0545

    adding likes functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MoreVert } from '@material-ui/icons';
 
 import './post.css';
 import { Users } from '../../dummydata';
 
 const Post = ({ comment, date, desc, like, photo, userId }) => {
+  const [likes, setLikes] = useState(like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = (e) => {
+    e.preventDefault();
+    setLikes(isLiked ? likes - 1 : likes + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className='post'>
       <div className='postWrapper'>
@@ -20,7 +29,7 @@ const Post = ({ comment, date, desc, like, photo, userId }) => {
             </span>
             <div className='postDate'>{date}</div>
           </div>
-          <div classNam e='postTopRight'>
+          <div className='postTopRight'>
             <MoreVert />
           </div>
         </div>
@@ -29,10 +38,10 @@ const Post = ({ comment, date, desc, like, photo, userId }) => {
           <img className='postImg' src={photo} alt='' />
         </div>
         <div className='postBottom'>
-          <div className='postBottomLeft'>
+          <div className='postBottomLeft' onClick={likeHandler}>
             <img className='likeIcon' src='/assets/like.png' alt='' />
             <img className='likeIcon' src='/assets/heart.png' alt='' />
-            <span className='postLikeCounter'>{like}</span>
+            <span className='postLikeCounter'>{likes}</span>
           </div>
           <div className='postBottomRight'>
             <span className='postCommentText'>{comment} comments</span>
